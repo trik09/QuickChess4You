6ffb47800dde05a5fb0d76202320c2fdd1f1ee88
Mohammad Farazuddin <mohammadfarazf7@gmail.com>
2026-05-14 13:24:49 +0530

    Competition Page UI Done

--- src/components/ChessBoard/ChessBoard.jsx
+++ src/components/ChessBoard/ChessBoard.jsx
@@ -1855,6 +1855,7 @@ function ChessBoard({
           overflow: 'hidden',
           position: 'relative'
         }}
+        className={styles.boardWrapper}
       >
         {feedback && (
           <div className={`${styles.feedback} ${styles[feedback]}`}>
